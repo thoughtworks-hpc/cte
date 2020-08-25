d7c4799f8016718bb57858eea34317b4258f931d
David Zhao <davidzwb@gmail.com>
2020-08-26 05:19:30 +0800

    Initialize order to send to match engine

--- order_manager/src/order_manager.cc
+++ order_manager/src/order_manager.cc
@@ -18,19 +18,22 @@
   time_stamp current_time_stamp =
       std::chrono::time_point_cast<std::chrono::nanoseconds>(
           std::chrono::system_clock::now());
+  int64_t nanoseconds_since_epoch =
+      current_time_stamp.time_since_epoch().count();
 
   influxdb_cpp::server_info si("127.0.0.1", 8086, "order_manager", "", "");
   std::string resp;
 
+  int64_t order_id = ++order_id_;
   int ret = influxdb_cpp::builder()
                 .meas("order")
-                .tag("order_id", std::to_string(++order_id_))
+                .tag("order_id", std::to_string(order_id))
                 .field("user_id", request->user_id())
                 .field("price", request->price())
                 .field("amount", request->amount())
                 .field("trading_side", request->trading_side())
                 .field("status", std::string("received"))
-                .timestamp(current_time_stamp.time_since_epoch().count())
+                .timestamp(nanoseconds_since_epoch)
                 .post_http(si, &resp);
 
   std::string message;
@@ -47,6 +50,17 @@
   }
 
   response->set_message(message);
+
+  order.set_order_id(order_id);
+  order.set_symbol(request->symbol());
+  order.set_user_id(request->user_id());
+  order.set_price(request->price());
+  order.set_amount(request->amount());
+  order.set_trading_side(static_cast<match_engine_proto::TradingSide>(
+      static_cast<int>(request->trading_side())));
+  google::protobuf::Timestamp submit_time;
+  submit_time.set_nanos(nanoseconds_since_epoch);
+  order.set_allocated_submit_time(&submit_time);
 
   //  stub_->Match(&client_context, order, reply);
   return grpc::Status::OK;
