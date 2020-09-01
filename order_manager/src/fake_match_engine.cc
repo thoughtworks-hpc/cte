f4ca888128980d4ce1921c89b736b150a4c46d01
David Zhao <davidzwb@gmail.com>
2020-09-01 16:45:28 +0800

    Add more print to FakeMatchEngineService

--- order_manager/src/fake_match_engine.cc
+++ order_manager/src/fake_match_engine.cc
@@ -9,7 +9,13 @@
 ::grpc::Status FakeMatchEngineService::Match(
     ::grpc::ServerContext *context, const ::match_engine_proto::Order *request,
     ::match_engine_proto::Reply *response) {
-  std::cout << "receive order " << request->order_id() << std::endl;
+  std::cout << "receive order:" << std::endl;
+  std::cout << "order id: " << request->order_id() << std::endl;
+  std::cout << "symbol: " << request->symbol() << std::endl;
+  std::cout << "submit_time: " << request->submit_time().DebugString();
+  std::cout << "price: " << request->price() << std::endl;
+  std::cout << "amount: " << request->amount() << std::endl;
+  std::cout << "trading_side: " << request->trading_side() << std::endl;
 
   response->set_status(match_engine_proto::STATUS_SUCCESS);
   response->set_message("all good");
@@ -30,6 +36,10 @@
 
   while (count_down--) {
     writer->Write(trade);
+    std::cout << "send trade back:" << std::endl;
+    std::cout << "amount: " << trade.amount() << std::endl;
+    std::cout << "maker_id: " << trade.maker_id() << std::endl;
+    std::cout << "taker_id: " << trade.taker_id() << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 
