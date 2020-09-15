bfa32391faa15b7ed5aa68bfe9a9de148314b197
yuecheng.pei <793499453@qq.com>
2020-09-15 16:13:54 +0800

    Add deployment folder

--- trade_persistence_tool/src/trade_persist_influxdb.cc
+++ trade_persistence_tool/src/trade_persist_influxdb.cc
@@ -13,13 +13,6 @@
 bool TradePersistInfluxdb::PersistTrade(TradeEntity& trade) {
   influxdb_cpp::server_info si(ip_, std::stoi(port_), database_name_, username_,
                                password_);
-  CDCF_LOGGER_DEBUG(
-      "Connecting database server {}:{} with username:\"{}\" , "
-      "password:\"{}\" ",
-      ip_, port_, username_, password_);
-  CDCF_LOGGER_DEBUG("Using database_name:\"{}\" , table_name:\"{}\"   ",
-                    database_name_, database_table_name_);
-
   std::string resp;
   int ret = influxdb_cpp::builder()
                 .meas(database_table_name_)
@@ -34,15 +27,14 @@ bool TradePersistInfluxdb::PersistTrade(TradeEntity& trade) {
                 .post_http(si, &resp);
 
   if (0 == ret && resp.empty()) {
-    CDCF_LOGGER_DEBUG("Write db:");
-    CDCF_LOGGER_DEBUG("buy_order_id: {}", trade.buy_order_id_);
-    CDCF_LOGGER_DEBUG("sell_order_id: {}", trade.sell_order_id_);
-    CDCF_LOGGER_DEBUG("symbol_id: {}", trade.symbol_id_);
-    CDCF_LOGGER_DEBUG("trade_id: {}", trade.trade_id_);
-    CDCF_LOGGER_DEBUG("price: {}", trade.price_);
-    CDCF_LOGGER_DEBUG("amount: {}", trade.amount_);
-    CDCF_LOGGER_DEBUG("sell_user_id: {}", trade.sell_user_id_);
-    CDCF_LOGGER_DEBUG("buy_user_id: {}", trade.buy_user_id_);
+    CDCF_LOGGER_DEBUG(
+        "Write #{}:  buy_order_id: {}, sell_order_id: {}, symbol_id: {}, "
+        "trade_id: {}, "
+        " price: {}, amount: {}, sell_user_id: {}, buy_user_id: {}",
+        ++count_, trade.buy_order_id_, trade.sell_order_id_, trade.symbol_id_,
+        trade.trade_id_, trade.price_, trade.amount_, trade.sell_user_id_,
+        trade.buy_user_id_);
+
     return true;
   } else {
     CDCF_LOGGER_ERROR("  Write db failed, ret:{} resp:{}", ret, resp);
@@ -55,6 +47,12 @@ int TradePersistInfluxdb::CreateDatabase() {
   int ret;
   influxdb_cpp::server_info si(ip_, std::stoi(port_), database_name_, username_,
                                password_);
+  CDCF_LOGGER_DEBUG(
+      "Connecting database server {}:{} with username:\"{}\" , "
+      "password:\"{}\" ",
+      ip_, port_, username_, password_);
+  CDCF_LOGGER_DEBUG("Using database_name:\"{}\" , table_name:\"{}\"   ",
+                    database_name_, database_table_name_);
   ret = influxdb_cpp::create_db(resp, database_name_, si);
   if (0 != ret) {
     CDCF_LOGGER_ERROR("Creat db failed ret:{}", ret);
