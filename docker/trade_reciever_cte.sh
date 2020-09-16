724eb1c65f29bfed463f293854c03f852efd7d7d
David Zhao <davidzwb@gmail.com>
2020-09-17 06:31:43 +0800

    Optimize demo usage

--- docker/trade_reciever_cte.sh
+++ docker/trade_reciever_cte.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 /usr/bin/influxd &
 sleep 10
-/bin/trade_persistence_tool_main -t cte_trades -m cte_match_engin_result_host:51001 -d 127.0.0.1:8086 -l debug -f /tmp/cte_trade_persistence_tool.log &
-/bin/trade_persistence_tool_main -t akka_te_trades -m akkagateway:50051 -d 127.0.0.1:8086 -l debug -f /tmp/akka_te_trade_persistence_tool.log
+/bin/trade_persistence_tool_main -t ${CTE_MEASUREMENT_NAME} -m cte_match_engin_result_host:51001 -d 127.0.0.1:8086 -l debug -f /tmp/cte_trade_persistence_tool.log &
+/bin/trade_persistence_tool_main -t ${AKKA_TE_MEASUREMENT_NAME} -m akkagateway:50051 -d 127.0.0.1:8086 -l debug -f /tmp/akka_te_trade_persistence_tool.log
