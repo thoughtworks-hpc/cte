830724a29b06ff146c84e3bc34db7afd64d4ae6a
yuecheng.pei <793499453@qq.com>
2020-09-17 15:43:45 +0800

    Change trade manager log file output path

--- deployment/start_trade_manager.sh
+++ deployment/start_trade_manager.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sleep 5
-/bin/trade_persistence_tool_main -t cte_trades -m $CTE_MATCH_ENGIN:51001 -d $DB_HOST:8086 -l debug -f /tmp/cte_trade_persistence_tool.log &
-/bin/trade_persistence_tool_main -t akka_te_trades -m $AKKA_MATCH_ENGIN:50051 -d $DB_HOST:8086 -l debug -f /tmp/akka_te_trade_persistence_tool.log
+/bin/trade_persistence_tool_main -t cte_trades -m $CTE_MATCH_ENGIN:51001 -d $DB_HOST:8086 -l debug -f /tmp/log/cte_trade_persistence_tool.log &
+/bin/trade_persistence_tool_main -t akka_te_trades -m $AKKA_MATCH_ENGIN:50051 -d $DB_HOST:8086 -l debug -f /tmp/log/akka_te_trade_persistence_tool.log
