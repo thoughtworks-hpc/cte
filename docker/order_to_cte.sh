61ce352f118769b174213b7eb4ec07784c7c4a0f
cx-Zh <491907188@qq.com>
2020-09-18 11:27:18 +0800

    Change config and add port in docker-compose

--- docker/order_to_cte.sh
+++ docker/order_to_cte.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 /usr/bin/influxd &
 sleep 5
-/bin/order_manager -a 0.0.0.0:50051 -d localhost:8086 -m cte_match_engin_result_host:51001 -t 1
+/bin/order_manager -a 0.0.0.0:50051 -d localhost:8086 -m cte_match_engin_result_host:51001 -t 0
