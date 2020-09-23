78294922c47151e8331a53f47c9e021e1acb7526
yuecheng.pei <793499453@qq.com>
2020-09-23 10:45:52 +0800

    Modify github action scroipts

--- deployment/trigger_github_deploy_workflow.sh
+++ deployment/trigger_github_deploy_workflow.sh
@@ -11,5 +11,5 @@ curl \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/thoughtworks-hpc/cte/actions/workflows/publish_and_deploy_image.yml/dispatches \
   -d '{"ref":"develop",
-  "inputs":{"DEBUG_FLAG": "100"
-  }}'
+  "inputs":{"DEBUG_FLAG": "100"}
+  }'
