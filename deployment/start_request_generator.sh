b2c9b65be11939f87ac6c1443d09eb178c2a4c88
yuecheng.pei <793499453@qq.com>
2020-09-24 16:23:12 +0800

    Change data_verify sleep time for DEBUG

--- deployment/start_request_generator.sh
+++ deployment/start_request_generator.sh
@@ -3,6 +3,6 @@
 cd bin
 /bin/request_generator_main -f test_env_cte_request_generator_config.json &
 /bin/request_generator_main -f test_env_akka_request_generator_config.json
-sleep 10m
+sleep 1m
 /tmp/data_verifier
 sleep infinity
