c52e918bc7d19e9916618cb6b15fcb53968ca0e7
David Zhao <davidzwb@gmail.com>
2020-10-15 11:35:42 +0800

    Upgrade cdcf version to 1.2.1

--- docker/script.sh
+++ docker/script.sh
@@ -5,5 +5,5 @@ echo "seeds: $SEEDS"
 NAME=${NAME:-$HOST}
 echo "name: ${NAME}"
 
-echo /bin/node_keeper --name=$NAME --seeds=$SEEDS --host=$HOST --port=4445 --role=$ROLE --app=$APP  --app-args="${APP_ARGS}"
-/bin/node_keeper --name=$NAME --seeds=$SEEDS --host=$HOST --port=4445 --role=$ROLE --app=$APP  --app-args="${APP_ARGS}"
+echo /bin/node_keeper --name=$NAME --seeds=$SEEDS --host=$HOST --port=4445 $LOG_COMMANDS --role=$ROLE --app=$APP  --app-args="${APP_ARGS}"
+/bin/node_keeper --name=$NAME --seeds=$SEEDS --host=$HOST --port=4445 $LOG_COMMANDS --role=$ROLE --app=$APP  --app-args="${APP_ARGS}"
