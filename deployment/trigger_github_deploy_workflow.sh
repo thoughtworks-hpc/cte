05a705fd5b9a7fce819b02c425251a52c540b23a
cx-Zh <491907188@qq.com>
2020-10-16 16:05:31 +0800

    Change branch name used in test env

--- deployment/trigger_github_deploy_workflow.sh
+++ deployment/trigger_github_deploy_workflow.sh
@@ -10,6 +10,6 @@ curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/thoughtworks-hpc/cte/actions/workflows/publish_and_deploy_image.yml/dispatches \
-  -d '{"ref": "prepare_for_deployment",
+  -d '{"ref": "long_run",
   "inputs": {"DEBUG_FLAG": "0", "NUM_OF_REQUEST": "1000"}
   }'
