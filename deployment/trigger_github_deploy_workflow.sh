35941040aa1f9797bedfca4a4145f3256f67477e
cx-Zh <491907188@qq.com>
2020-10-21 15:08:58 +0800

    Change order amount bigger for test

--- deployment/trigger_github_deploy_workflow.sh
+++ deployment/trigger_github_deploy_workflow.sh
@@ -11,5 +11,5 @@ curl \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/thoughtworks-hpc/cte/actions/workflows/publish_and_deploy_image.yml/dispatches \
   -d '{"ref": "long_run",
-  "inputs": {"DEBUG_FLAG": "0", "NUM_OF_REQUEST": "1000", "SKIP_PUBLISH_DOCKER_IMAGE": "false"}
+  "inputs": {"DEBUG_FLAG": "0", "NUM_OF_REQUEST": "100000", "SKIP_PUBLISH_DOCKER_IMAGE": "false"}
   }'
