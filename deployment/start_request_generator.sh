2f276c2538083fc35502d68c2fe60176eb4f42f1
yuecheng.pei <793499453@qq.com>
2020-10-21 14:40:19 +0800

    Optimize request generator shell script

--- deployment/start_request_generator.sh
+++ deployment/start_request_generator.sh
@@ -27,43 +27,43 @@ while true; do
   /bin/request_generator_main -n $NUM_OF_REQUEST -f test_env_akka_request_generator_config.json
   echo '[IMPORTANT] akka-te is finished, waiting for database service'
 
-  while true
-  do
-    PID_EXIST=$(ps aux | awk '{print $2}'| grep -w $cte_pid)
-    if [ ! $PID_EXIST ]
-    then
-      echo '[IMPORTANT] cte is finished, waiting for database service'
-      break
-    fi
-    echo 'cte is still running, try sleep 5 seconds...'
-    sleep 5
-  done
+while true
+do
+  PID_EXIST=$(ps aux | awk '{print $2}'| grep -w $cte_pid)
+  if [ ! $PID_EXIST ]
+  then
+    echo '[IMPORTANT] cte is finished, waiting for database service'
+    break
+  fi
+  echo 'cte is still running, try sleep 5 seconds...'
+  sleep 30
+done
 
-  while true
-  do
-    count1=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(*) FROM cte_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
-    sleep 5
-    count2=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(*) FROM cte_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
-    if [ $count1 == $count2 ]
-    then
-      echo '[IMPORTANT] cte database is available now'
-      break
-    fi
-    echo 'cte database is still busy, try to sleep 5 seconds...'
-  done
+while true
+do
+  count1=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM cte_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
+  sleep 30
+  count2=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM cte_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
+  if [ $count1 == $count2 ]
+  then
+    echo '[IMPORTANT] cte database is available now'
+    break
+  fi
+  echo 'cte database is still busy, try to sleep 30 seconds...'
+done
 
-  while true
-  do
-    count1=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(*) FROM akka_te_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
-    sleep 5
-    count2=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(*) FROM akka_te_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
-    if [ $count1 == $count2 ]
-    then
-      echo '[IMPORTANT] akka_te database is available now'
-      break
-    fi
-    echo 'akka_te database is still busy, try to sleep 5 seconds...'
-  done
+while true
+do
+  count1=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM akka_te_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
+  sleep 30
+  count2=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM akka_te_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
+  if [ $count1 == $count2 ]
+  then
+    echo '[IMPORTANT] akka_te database is available now'
+    break
+  fi
+  echo 'akka_te database is still busy, try to sleep 30 seconds...'
+done
 
   echo '[IMPORTANT] start data_verifier now'
   cd /tmp
