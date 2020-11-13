df74bbc9b230ad26deb278c7066ce6e241b644d9
yuecheng.pei <793499453@qq.com>
2020-11-13 14:01:59 +0800

    Refactoring trade manager code

--- trade_persistence_tool/include/config.h
+++ trade_persistence_tool/include/config.h
@@ -17,22 +17,22 @@
 extern std::vector<TradeEntity> trade_manager_db_buffer;
 extern std::mutex trade_manager_db_buffer_mutex;
 
-    namespace trade_persistence_tool {
-  class Config {
-   public:
-    std::string db_ip;
-    std::string db_port;
-    std::string database_name;
-    std::string database_table_name;
-    std::string order_manager_address;
-    std::string db_username;
-    std::string db_password;
-    cdcf::CDCFConfig cdcf_config;
+namespace trade_persistence_tool {
+class Config {
+ public:
+  std::string db_ip;
+  std::string db_port;
+  std::string database_name;
+  std::string database_table_name;
+  std::string order_manager_address;
+  std::string db_username;
+  std::string db_password;
+  cdcf::CDCFConfig cdcf_config;
 
-    Config(int argc, char* argv[]);
-  };
+  Config(int argc, char* argv[]);
+};
 
-  std::vector<std::string> ParseIpAddress(std::string address);
+std::vector<std::string> ParseIpAddress(std::string address);
 
 }  // namespace trade_persistence_tool
 #endif  // TRADE_PERSISTENCE_TOOL_INCLUDE_CONFIG_H_
--- trade_persistence_tool/src/main.cc
+++ trade_persistence_tool/src/main.cc
@@ -12,6 +12,9 @@
 static bool thread_end_flag;
 static std::mutex thread_end_flag_mutex;
 
+bool write_data_to_db(int count_, trade_persistence_tool::Config& config,
+                      influxdb_cpp::server_info& si);
+
 int main(int argc, char* argv[]) {
   trade_persistence_tool::Config config(argc, argv);
 
@@ -22,6 +25,7 @@ int main(int argc, char* argv[]) {
       influxdb, config.order_manager_address, config.database_table_name);
 
   std::thread th = std::thread([&] {
+    CDCF_LOGGER_INFO("Chunk Database IO Thread starts");
     influxdb_cpp::server_info si(config.db_ip, std::stoi(config.db_port),
                                  config.database_name, config.db_username,
                                  config.db_password);
@@ -30,53 +34,66 @@ int main(int argc, char* argv[]) {
       thread_end_flag_mutex.lock();
       if (thread_end_flag) {
         trade_manager_db_buffer_mutex.lock();
-
+        write_data_to_db(count_, config, si);
         trade_manager_db_buffer_mutex.unlock();
+        CDCF_LOGGER_INFO("Chunk Database IO Thread ends");
         return;
       }
       thread_end_flag_mutex.unlock();
 
       trade_manager_db_buffer_mutex.lock();
       if (trade_manager_db_buffer.size() >= 5000) {
-        CDCF_LOGGER_DEBUG("Try to send buffered trades data to influxdb");
-        std::string resp;
-        auto payload = influxdb_cpp::builder();
-        for (const auto& trade : trade_manager_db_buffer) {
-          payload.meas(config.database_name)
-              .tag("buy_order_id", trade.buy_order_id_)
-              .tag("sell_order_id", trade.sell_order_id_)
-              .field("symbol_id", trade.symbol_id_)
-              .field("trade_id", trade.trade_id_)
-              .field("price", trade.price_)
-              .field("amount", trade.amount_)
-              .field("sell_user_id", trade.sell_user_id_)
-              .field("buy_user_id", trade.buy_user_id_)
-              .field("submit_time", static_cast<int64_t>(trade.submit_time));
-        }
-        int ret = payload.chunk_post(si, &resp);
-
-        if (0 == ret && resp.empty()) {
-          for (const auto& trade : trade_manager_db_buffer) {
-            CDCF_LOGGER_DEBUG(
-                "Write #{}:  buy_order_id: {}, sell_order_id: {}, symbol_id: {}, "
-                "trade_id: {}, "
-                " price: {}, amount: {}, sell_user_id: {}, buy_user_id: {}",
-                ++count_, trade.buy_order_id_, trade.sell_order_id_,
-                trade.symbol_id_, trade.trade_id_, trade.price_, trade.amount_,
-                trade.sell_user_id_, trade.buy_user_id_);
-          }
-        } else {
-          CDCF_LOGGER_ERROR("  Write db failed, ret:{} resp:{}", ret, resp);
+        if (!write_data_to_db(count_, config, si)) {
           return;
         }
+        trade_manager_db_buffer_mutex.unlock();
+        sleep(30);
       }
-      trade_manager_db_buffer_mutex.unlock();
-      sleep(30);
     }
-});
+  });
 
   tradePersistenceClient.PersistTrades();
   thread_end_flag = true;
   th.join();
   CDCF_LOGGER_CRITICAL("Trade Manager Shut Down");
+}
+
+bool write_data_to_db(int count_, trade_persistence_tool::Config& config,
+                      influxdb_cpp::server_info& si) {
+  trade_manager_db_buffer_mutex.lock();
+  CDCF_LOGGER_DEBUG("Try to send buffered trades data to influxdb");
+  std::string resp;
+  auto payload = influxdb_cpp::builder();
+  for (const auto& trade : trade_manager_db_buffer) {
+    payload.meas(config.database_name)
+        .tag("buy_order_id", trade.buy_order_id_)
+        .tag("sell_order_id", trade.sell_order_id_)
+        .field("symbol_id", trade.symbol_id_)
+        .field("trade_id", trade.trade_id_)
+        .field("price", trade.price_)
+        .field("amount", trade.amount_)
+        .field("sell_user_id", trade.sell_user_id_)
+        .field("buy_user_id", trade.buy_user_id_)
+        .field("submit_time", static_cast<int64_t>(trade.submit_time));
+  }
+  int ret = payload.chunk_post(si, &resp);
+
+  if (0 == ret && resp.empty()) {
+    for (const auto& trade : trade_manager_db_buffer) {
+      CDCF_LOGGER_DEBUG(
+          "Write #{}:  buy_order_id: {}, sell_order_id: {}, symbol_id: "
+          "{}, "
+          "trade_id: {}, "
+          " price: {}, amount: {}, sell_user_id: {}, buy_user_id: {}",
+          ++count_, trade.buy_order_id_, trade.sell_order_id_, trade.symbol_id_,
+          trade.trade_id_, trade.price_, trade.amount_, trade.sell_user_id_,
+          trade.buy_user_id_);
+    }
+  } else {
+    CDCF_LOGGER_ERROR("Write db failed, ret:{} resp:{}", ret, resp);
+    trade_manager_db_buffer_mutex.unlock();
+    return false;
+  }
+  trade_manager_db_buffer_mutex.unlock();
+  return true;
 }
