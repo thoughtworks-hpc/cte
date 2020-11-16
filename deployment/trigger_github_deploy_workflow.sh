4d503895aedba8e7ae656b7252afcd9529c23398
cx-Zh <491907188@qq.com>
2020-11-16 12:58:33 +0800

    Delete cte sub amtch engine

--- deployment/trigger_github_deploy_workflow.sh
+++ deployment/trigger_github_deploy_workflow.sh
@@ -11,5 +11,5 @@ curl \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/thoughtworks-hpc/cte/actions/workflows/publish_and_deploy_image.yml/dispatches \
   -d '{"ref": "test-perf",
-  "inputs": {"DEBUG_FLAG": "1", "NUM_OF_REQUEST": "800000", "SKIP_PUBLISH_DOCKER_IMAGE": "true"}
+  "inputs": {"DEBUG_FLAG": "1", "NUM_OF_REQUEST": "500000", "SKIP_PUBLISH_DOCKER_IMAGE": "false"}
   }'
