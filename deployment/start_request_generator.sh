a24004616c1890e0cb086fd364fc7a073d703530
yuecheng.pei <793499453@qq.com>
2020-11-26 15:32:24 +0800

    Add more log to check io

--- deployment/start_request_generator.sh
+++ deployment/start_request_generator.sh
@@ -5,6 +5,7 @@ while true; do
   #  curl -POST http://172.30.28.8:8086/query -s --data-urlencode "q=DROP DATABASE order_manager"
   #  curl -POST http://172.30.28.8:8086/query -s --data-urlencode "q=DROP DATABASE trade_manager"
 
+  # 初始化
   echo "[$(date "+%Y-%m-%d %T.%3N")] [info] round $i starts"
   echo "[$(date "+%Y-%m-%d %T.%3N")] [info] round $i starts" >>/tmp/log/long_run_status.log
 
@@ -38,12 +39,14 @@ while true; do
   echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start generating $NUM_OF_REQUEST requests to cte and akka-te"
   echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start generating $NUM_OF_REQUEST requests to cte and akka-te" >>/tmp/log/long_run_status.log
 
+  # 启动请求模拟器
   /bin/request_generator_main -n $NUM_OF_REQUEST -f test_env_cte_request_generator_config.json &
   cte_pid=$!
   /bin/request_generator_main -n $NUM_OF_REQUEST -f test_env_akka_request_generator_config.json
   echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka-te is finished, waiting for database service"
   echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka-te is finished, waiting for database service" >>/tmp/log/long_run_status.log
 
+  # 判断 cte 撮合是否结束
   while true; do
     PID_EXIST=$(ps aux | awk '{print $2}' | grep -w $cte_pid)
     if [ ! $PID_EXIST ]; then
@@ -56,6 +59,7 @@ while true; do
     sleep 30
   done
 
+  # 当 cte 撮合结束，开始判断 cte 是否写库结束
   while true; do
     count1=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM cte_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
     sleep 15
@@ -69,6 +73,7 @@ while true; do
     echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte database is still busy, try to sleep 15 seconds..." >>/tmp/log/long_run_status.log
   done
 
+  # 判断 akka 是否写库结束
   while true; do
     count1=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM akka_te_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
     sleep 15
@@ -82,6 +87,7 @@ while true; do
     echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka_te database is still busy, try to sleep 15 seconds..." >>/tmp/log/long_run_status.log
   done
 
+  # 启动数据验证器
   echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start data_verifier now"
   echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start data_verifier now" >>/tmp/log/long_run_status.log
   cd /tmp
@@ -130,6 +136,7 @@ while true; do
     fi
   done
 
+  # 备份数据库
   temp_num1=$(($i % 5))
   temp_num2=$(($i / 5))
   if [[ $temp_num1 -eq 0 && $temp_num2 -gt 2 ]]; then
