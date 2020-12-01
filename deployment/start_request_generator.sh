cd542e757509c7eefc8e8a09579d6a9c1f1c201f
David Zhao <davidzwb@gmail.com>
2020-12-01 11:06:09 +0800

    Add timestamp to each script output

--- deployment/start_request_generator.sh
+++ deployment/start_request_generator.sh
@@ -5,8 +5,8 @@ while true; do
   #  curl -POST http://172.30.28.8:8086/query -s --data-urlencode "q=DROP DATABASE order_manager"
   #  curl -POST http://172.30.28.8:8086/query -s --data-urlencode "q=DROP DATABASE trade_manager"
 
-  echo "[IMPORTANT] round $i starts"
-  echo "[IMPORTANT] round $i starts" >>/tmp/log/long_run_status.log
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] round $i starts"
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] round $i starts" >>/tmp/log/long_run_status.log
 
   if [[ $i -eq 0 ]]; then
     curl -POST http://172.30.28.30:8086/query -s --data-urlencode "q=DROP DATABASE orders"
@@ -21,38 +21,38 @@ while true; do
 
   cd /bin
   /bin/create_initial_prices
-  echo '[IMPORTANT] initial prices are generated'
-  echo '[IMPORTANT] initial prices are generated' >>/tmp/log/long_run_status.log
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] initial prices are generated"
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] initial prices are generated" >>/tmp/log/long_run_status.log
   /bin/create_orders initial_prices.json test_env_create_orders_config.json
-  echo '[IMPORTANT] initial orders are generated'
-  echo '[IMPORTANT] initial orders are generated' >>/tmp/log/long_run_status.log
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] initial orders are generated"
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] initial orders are generated" >>/tmp/log/long_run_status.log
 
   cte_trades_count=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM cte_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
   akka_te_trades_count=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM akka_te_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
 
-  echo "[IMPORTANT] cte_trades_count: ${cte_trades_count}"
-  echo "[IMPORTANT] cte_trades_count: ${cte_trades_count}" >>/tmp/log/long_run_status.log
-  echo "[IMPORTANT] akka_te_trades_count: ${akka_te_trades_count}"
-  echo "[IMPORTANT] akka_te_trades_count: ${akka_te_trades_count}" >>/tmp/log/long_run_status.log
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte_trades_count: ${cte_trades_count}"
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte_trades_count: ${cte_trades_count}" >>/tmp/log/long_run_status.log
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka_te_trades_count: ${akka_te_trades_count}"
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka_te_trades_count: ${akka_te_trades_count}" >>/tmp/log/long_run_status.log
 
-  echo "[IMPORTANT] start generating $NUM_OF_REQUEST requests to cte and akka-te"
-  echo "[IMPORTANT] start generating $NUM_OF_REQUEST requests to cte and akka-te" >>/tmp/log/long_run_status.log
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start generating $NUM_OF_REQUEST requests to cte and akka-te"
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start generating $NUM_OF_REQUEST requests to cte and akka-te" >>/tmp/log/long_run_status.log
 
   /bin/request_generator_main -n $NUM_OF_REQUEST -f test_env_cte_request_generator_config.json &
   cte_pid=$!
   /bin/request_generator_main -n $NUM_OF_REQUEST -f test_env_akka_request_generator_config.json
-  echo '[IMPORTANT] akka-te is finished, waiting for database service'
-  echo '[IMPORTANT] akka-te is finished, waiting for database service' >>/tmp/log/long_run_status.log
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka-te is finished, waiting for database service"
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka-te is finished, waiting for database service" >>/tmp/log/long_run_status.log
 
   while true; do
     PID_EXIST=$(ps aux | awk '{print $2}' | grep -w $cte_pid)
     if [ ! $PID_EXIST ]; then
-      echo '[IMPORTANT] cte is finished, waiting for database service'
-      echo '[IMPORTANT] cte is finished, waiting for database service' >>/tmp/log/long_run_status.log
+      echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte is finished, waiting for database service"
+      echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte is finished, waiting for database service" >>/tmp/log/long_run_status.log
       break
     fi
-    echo 'cte is still running, try sleep 30 seconds...'
-    echo 'cte is still running, try sleep 30 seconds...' >>/tmp/log/long_run_status.log
+    echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte is still running, try sleep 30 seconds..."
+    echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte is still running, try sleep 30 seconds..." >>/tmp/log/long_run_status.log
     sleep 30
   done
 
@@ -61,12 +61,12 @@ while true; do
     sleep 15
     count2=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM cte_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
     if [ $count1 == $count2 ]; then
-      echo '[IMPORTANT] cte database is available now'
-      echo '[IMPORTANT] cte database is available now' >>/tmp/log/long_run_status.log
+      echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte database is available now"
+      echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte database is available now" >>/tmp/log/long_run_status.log
       break
     fi
-    echo 'cte database is still busy, try to sleep 15 seconds...'
-    echo 'cte database is still busy, try to sleep 15 seconds...' >>/tmp/log/long_run_status.log
+    echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte database is still busy, try to sleep 15 seconds..."
+    echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte database is still busy, try to sleep 15 seconds..." >>/tmp/log/long_run_status.log
   done
 
   while true; do
@@ -74,16 +74,16 @@ while true; do
     sleep 15
     count2=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM akka_te_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
     if [ $count1 == $count2 ]; then
-      echo '[IMPORTANT] akka_te database is available now'
-      echo '[IMPORTANT] akka_te database is available now' >>/tmp/log/long_run_status.log
+      echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka_te database is available now"
+      echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka_te database is available now" >>/tmp/log/long_run_status.log
       break
     fi
-    echo 'akka_te database is still busy, try to sleep 15 seconds...'
-    echo 'akka_te database is still busy, try to sleep 15 seconds...' >>/tmp/log/long_run_status.log
+    echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka_te database is still busy, try to sleep 15 seconds..."
+    echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka_te database is still busy, try to sleep 15 seconds..." >>/tmp/log/long_run_status.log
   done
 
-  echo '[IMPORTANT] start data_verifier now'
-  echo '[IMPORTANT] start data_verifier now' >>/tmp/log/long_run_status.log
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start data_verifier now"
+  echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start data_verifier now" >>/tmp/log/long_run_status.log
   cd /tmp
   /tmp/data_verifier
   result=$?
@@ -105,13 +105,13 @@ while true; do
   curl -POST 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode 'db=trade_manager' -s --data-urlencode "q=select * into akka_te_trades_backup_${i} from akka_te_trades"
 
   while true; do
-    echo '[IMPORTANT] start to clear akka_te_trades from trade_manager database'
-    echo '[IMPORTANT] start to clear akka_te_trades from trade_manager database' >>/tmp/log/long_run_status.log
+    echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start to clear akka_te_trades from trade_manager database"
+    echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start to clear akka_te_trades from trade_manager database" >>/tmp/log/long_run_status.log
     curl -POST 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode 'db=trade_manager' -s --data-urlencode "q=drop measurement akka_te_trades"
     count=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM akka_te_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
     if [ -z "$count" ]; then
-      echo '[IMPORTANT] akka_te_trades cleared'
-      echo '[IMPORTANT] akka_te_trades cleared' >>/tmp/log/long_run_status.log
+      echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka_te_trades cleared"
+      echo "[$(date "+%Y-%m-%d %T.%3N")] [info] akka_te_trades cleared" >>/tmp/log/long_run_status.log
       break
     fi
   done
@@ -119,13 +119,13 @@ while true; do
   curl -POST 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode 'db=trade_manager' -s --data-urlencode "q=select * into cte_trades_backup_${i} from cte_trades"
 
   while true; do
-    echo '[IMPORTANT] start to clear cte_trades from trade_manager database'
-    echo '[IMPORTANT] start to clear cte_trades from trade_manager database' >>/tmp/log/long_run_status.log
+    echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start to clear cte_trades from trade_manager database"
+    echo "[$(date "+%Y-%m-%d %T.%3N")] [info] start to clear cte_trades from trade_manager database" >>/tmp/log/long_run_status.log
     curl -POST 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode 'db=trade_manager' -s --data-urlencode "q=drop measurement cte_trades"
     count=$(curl -GET 'http://172.30.28.30:8086/query?pretty=true' -s --data-urlencode "db=trade_manager" --data-urlencode "q=SELECT count(symbol_id) FROM cte_trades" | python -c 'import json,sys;obj=json.load(sys.stdin); print(obj["results"][0]["series"][0]["values"][0][1])')
     if [ -z "$count" ]; then
-      echo '[IMPORTANT] cte_trades cleared'
-      echo '[IMPORTANT] cte_trades cleared' >>/tmp/log/long_run_status.log
+      echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte_trades cleared"
+      echo "[$(date "+%Y-%m-%d %T.%3N")] [info] cte_trades cleared" >>/tmp/log/long_run_status.log
       break
     fi
   done
